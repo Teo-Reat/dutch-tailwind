ee62962d94661b9f7e7e664c85d69060ca747267
Teo-Reat <teo.reat@gmail.com>
2020-12-22 12:55:41 +0300

    Complete before purge

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '.splide', {
+		rewind: true,
+		fixedHeight: '500px',
+	} ).mount();
+} );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	purge: {
 		mode: 'layers',
-		content: ['./public/**/*.html/'],
+		content: ['./public/*.html/'],
 	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
